45c7bc6fb973dcd1f65b3b49dae80e9d06ea37f3
MalekQumboz <malek.hossam.209@gmail.com>
2023-01-27 15:47:38 +0200

    validate for employee

--- human_resource/human_resource/doctype/employee/employee.js
+++ human_resource/human_resource/doctype/employee/employee.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee', {
-	// refresh: function(frm) {
-
-	// }
+	 refresh: function(frm) {
+        frm.set_df_property('age', 'read_only','1');
+        frm.set_df_property('full_name', 'read_only','1');
+	 }
 });
